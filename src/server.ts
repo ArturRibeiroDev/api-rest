35df16fb9f916581fba0175a582dc7245c96f829
Artur Ribeiro Vieira Lima <109690783+ArturRibeiroDev@users.noreply.github.com>
2026-04-05 17:40:09 -0300

    chore: add .gitignore and untrack node_modules

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ app.use((error: any, request: Request , response: Response, _: NextFunction) => 
   }
 
   if(error instanceof ZodError) {
-    response.status(400).json({ message: "Validation Error!", issues: error.format() })
+    return response.status(400).json({ message: "Validation Error!", issues: error.format() })
   }
 
   response.status(500).json({ message: error.message })
